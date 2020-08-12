53824cff71054abf1fc3b07641405b7984b1c8e7
chen <>
2020-08-13 08:56:20 +0900

    add menu slide animation

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,43 +1,47 @@
 <template>
   <div class="ebook">
-      <div class="title-wapper">
-        <div class="left">
-          <span class="icon-back icon"></span>
+      <transition name="slide-down">
+        <div class="title-wapper" v-show="ifTitleAndMenuShow">
+          <div class="left">
+            <span class="icon-back icon"></span>
+          </div>
+          <div class="right">
+            <div class="icon-wapper">
+              <span class="icon-cart icon"></span>
+            </div>
+            <div class="icon-wapper">
+              <span class="icon-person icon"></span>
+            </div>
+            <div class="icon-wapper">
+              <span class="icon-more icon"></span>
+            </div>
+          </div>
         </div>
-        <div class="right">
-          <div class="icon-wapper">
-            <span class="icon-cart icon"></span>
-          </div>
-          <div class="icon-wapper">
-            <span class="icon-person icon"></span>
-          </div>
-          <div class="icon-wapper">
-            <span class="icon-more icon"></span>
-          </div>
-        </div>
-      </div>
+      </transition>
       <div class="read-wapper">
           <div id="read"></div>
           <div class="mask">
             <div class="left" @click="prevPage"></div>
-            <div class="center"></div>
+            <div class="center" @click="toggleTitleAndMenu"></div>
             <div class="right" @click="nextPage"></div>
           </div>
       </div>
-      <div class="menu-wapper">
-        <div class="icon-wapper">
-          <span class="icon-menu icon"></span>
+      <transition name="slide-up">
+        <div class="menu-wapper" v-show="ifTitleAndMenuShow">
+          <div class="icon-wapper">
+            <span class="icon-menu icon"></span>
+          </div>
+          <div class="icon-wapper">
+            <span class="icon-progress icon"></span>
+          </div>
+          <div class="icon-wapper">
+            <span class="icon-bright icon"></span>
+          </div>
+          <div class="icon-wapper">
+            <span class="icon-a icon">A</span>
+          </div>
         </div>
-        <div class="icon-wapper">
-          <span class="icon-progress icon"></span>
-        </div>
-        <div class="icon-wapper">
-          <span class="icon-bright icon"></span>
-        </div>
-        <div class="icon-wapper">
-          <span class="icon-a icon">A</span>
-        </div>
-      </div>
+      </transition>
   </div>
 </template>
 
@@ -46,10 +50,18 @@ import Epub from 'epubjs'
 const DOWNLOAD_URL = './static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 global.ePub = Epub
 export default {
+  data() {
+    return {
+      ifTitleAndMenuShow: false
+    }
+  },
   mounted() {
     this.showEpub()
   },
   methods: {
+    toggleTitleAndMenu() {
+      this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+    },
     prevPage() {
       // redition.prev
       if (this.rendition) {
