14ce10c18b2773c97d74f582ae5103e042b1249e
chen <>
2020-08-14 11:45:45 +0900

    set fontSizr

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="ebook">
-      <transition name="slide-down">
-        <div class="title-wapper" v-show="ifTitleAndMenuShow">
-          <div class="left">
-            <span class="icon-back icon"></span>
-          </div>
-          <div class="right">
-            <div class="icon-wapper">
-              <span class="icon-cart icon"></span>
-            </div>
-            <div class="icon-wapper">
-              <span class="icon-person icon"></span>
-            </div>
-            <div class="icon-wapper">
-              <span class="icon-more icon"></span>
-            </div>
-          </div>
-        </div>
-      </transition>
-      <div class="read-wapper">
+      <title-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></title-bar>
+      <div class="read-wrapper">
           <div id="read"></div>
           <div class="mask">
             <div class="left" @click="prevPage"></div>
@@ -26,41 +9,52 @@
             <div class="right" @click="nextPage"></div>
           </div>
       </div>
-      <transition name="slide-up">
-        <div class="menu-wapper" v-show="ifTitleAndMenuShow">
-          <div class="icon-wapper">
-            <span class="icon-menu icon"></span>
-          </div>
-          <div class="icon-wapper">
-            <span class="icon-progress icon"></span>
-          </div>
-          <div class="icon-wapper">
-            <span class="icon-bright icon"></span>
-          </div>
-          <div class="icon-wapper">
-            <span class="icon-a icon">A</span>
-          </div>
-        </div>
-      </transition>
+      <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow" :fontSizeList="fontSizeList"
+      :defaultFontSize="defaultFontSize" @setFontSize="setFontSize" ref="MenuBar"></menu-bar>
   </div>
 </template>
 
 <script>
+import TitleBar from '@/components/TitleBar'
+import MenuBar from '@/components/MenuBar'
 import Epub from 'epubjs'
 const DOWNLOAD_URL = './static/2018_Book_AgileProcessesInSoftwareEngine.epub'
 global.ePub = Epub
 export default {
+  components: {
+    TitleBar,
+    MenuBar
+  },
   data() {
     return {
-      ifTitleAndMenuShow: false
+      ifTitleAndMenuShow: false,
+      fontSizeList: [
+        {fontSize: 12},
+        {fontSize: 14},
+        {fontSize: 16},
+        {fontSize: 18},
+        {fontSize: 20},
+        {fontSize: 22},
+        {fontSize: 24}
+      ],
+      defaultFontSize: 16
     }
   },
   mounted() {
     this.showEpub()
   },
   methods: {
+    setFontSize(fontSize) {
+      this.defaultFontSize = fontSize
+      if (this.themes) {
+        this.themes.fontSize(fontSize + 'px')
+      }
+    },
     toggleTitleAndMenu() {
       this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (!this.ifTitleAndMenuShow) {
+        this.$refs.MenuBar.hideFontSetting()
+      }
     },
     prevPage() {
       // redition.prev
@@ -85,6 +79,8 @@ export default {
       })
       // using rendition.display to display book
       this.rendition.display()
+      this.themes = this.rendition.themes
+      this.setFontSize(this.defaultFontSize)
     }
   }
 }
@@ -94,31 +90,8 @@ export default {
 @import 'assets/styles/global';
 .ebook{
   position: relative;
-  .title-wapper {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 101;
-    display: flex;
-    width: 100%;
-    height: px2rem(48);
-    background:white;
-    box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
-    .left {
-      flex: 0 0 px2rem(60);
-      @include center;
-    }
-    .right {
-      flex: 1;
-      display: flex;
-      justify-content: flex-end;
-      .icon-wapper {
-        flex: 0 0 px2rem(40);
-        @include center;
-      }
-    }
-  }
-  .read-wapper {
+
+  .read-wrapper {
     .mask{
       position:absolute;
       top: 0;
@@ -138,26 +111,6 @@ export default {
       }
     }
   }
-  .menu-wapper {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    z-index: 101;
-    display: flex;
-    width: 100%;
-    height: px2rem(48);
-    background:white;
-    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
-    .icon-wapper {
-      flex: 1;
-      @include center;
-      .icon-progress {
-        font-size: px2rem(28);
-      }
-      .icon-bright {
-        font-size: px2rem(24);
-      }
-    }
-  }
+
 }
 </style>
